9704b4e59605c174fa39fd58226671d62520ac19
kty2001 <tyyoujungzz@naver.com>
2026-09-18 09:22:20 +0900

    Revert "Re: Initial commit"
    This reverts commit 17e7dccc1ce41d5d5643bde4db80937824189725.

--- Presentation/dist/assets/slidev/default-Duhuo5LY.js
+++
@@ -1,1 +0,0 @@
-import{I as e,v as t,z as n}from"../modules/shiki-DWY-nNAj.js";import{st as r}from"../index-DTiRYTjd.js";var i={},a={class:`slidev-layout default`};function o(r,i){return e(),t(`div`,a,[n(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
